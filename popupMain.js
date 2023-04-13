1c3701419ac839864cc4ea5b0125dcab38a6c90d
reflect6 <121671246+reflect6@users.noreply.github.com>
2023-04-13 06:51:40 -0400

    Add files via upload

--- popupMain.js
+++ popupMain.js
@@ -1,59 +1,162 @@
-/* Add this JavaScript to your popupMain.js file */
-document.addEventListener('DOMContentLoaded', function() {
-  var loginForm = document.getElementById('login-form');
-  var emailInput = document.getElementById('email');
-  var passwordInput = document.getElementById('password');
-  var spinner = document.getElementById('spinner');
-  var checkmark = document.getElementById('checkmark');
-  var crossmark = document.getElementById('crossmark');
-  var messageDiv = document.getElementById('message');
 
-  loginForm.addEventListener('submit', function(event) {
-    event.preventDefault();
+const storedToken = localStorage.getItem('myToken');
 
-    async function getPublicIpAddress() {
-      const response = await fetch('https://api.ipify.org?format=json');
-      const data = await response.json();
-      const ipAddress = data.ip;
-      return ipAddress;
-    }
 
-    async function setUserIpAddress() {
-      const userIpAddress = await getPublicIpAddress();
-      var userEmail = emailInput.value;
-      var userPassword = passwordInput.value;
 
-      // Show the loading spinner
-      spinner.style.display = 'block';
+async function getPublicIpAddress() {
+  const response = await fetch('https://api.ipify.org?format=json');
+  const data = await response.json();
+  const ipAddress = data.ip;
+  return ipAddress;
+}
 
-      // Send a request to your backend server to check if the user's email and IP address are stored in your SQL database
-      fetch('http://localhost:3000/login', {
+function showAlert(message) {
+  // Get a reference to the alert box
+  const alertBox = document.getElementById('custom-alert');
+
+  // Set the message text
+  alertBox.querySelector('.message').textContent = message;
+
+  // Show the alert box
+  alertBox.style.display = 'block';
+
+  // Add a click event listener to the OK button
+  alertBox.querySelector('.ok-button').addEventListener('click', hideAlert);
+
+  // Hide the alert box after 3 seconds
+  setTimeout(hideAlert, 3000);
+
+  function hideAlert() {
+    // Remove the event listener from the OK button
+    alertBox.querySelector('.ok-button').removeEventListener('click', hideAlert);
+
+    // Hide the alert box
+    alertBox.style.display = 'none';
+    window.location.href = "popupMain.html";
+  }
+}
+
+async function storeToken() {
+  fetch('http://localhost:3000/storeToken', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ email: userEmail, password: userPassword, ip: userIpAddress })
+        body: JSON.stringify({
+          token: storedToken
+        })
       })
-      .then(function(response) {
+      .then(function (response) {
         return response.json();
       })
-      .then(function(data) {
-        // Hide the loading spinner
-        spinner.style.display = 'none';
-
+      .then(function (data) {
         if (data.success) {
-          // The user is logged in, display a success message and show the checkmark
-          checkmark.style.display = 'block';
-        } else {
-          // The user is not logged in, display an error message and show the X mark
-          crossmark.style.display = 'block';
+          console.log('saved Token in SQL')
+        }else {
+          return false;
         }
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.error('Error:', error);
       });
-    }
+};
 
-    setUserIpAddress();
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  async function checkToken() {
+    const userIpAddress = await getPublicIpAddress();
+    const storedToken = localStorage.getItem('myToken');
+    console.log('User IP address:', userIpAddress);
+    console.log('Stored token:', storedToken);
+    fetch('http://localhost:3000/checkToken', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          ip: userIpAddress,
+          token: storedToken
+        })
+      })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log('Response from server:', data);
+        if (data.success) {
+          // The user is logged in, display a success message
+          console.log('User is logged in. Redirecting to popup.html...');
+          window.location.href = "popup.html";
+        } else {
+          // The user is not logged in, display the login form
+          console.log('User is not logged in. Displaying login form...');
+          var loginForm = document.getElementById('login-box');
+          var emailInput = document.getElementById('email');
+          var passwordInput = document.getElementById('password');
+          var loadingGif = document.getElementById('loading-container-gif');
+
+          loginForm.style.display = 'block';
+          loadingGif.style.display = 'none';
+
+          const inputs = document.querySelectorAll(".login-box .user-box input");
+
+          inputs.forEach((input) => {
+            input.addEventListener("input", () => {
+              if (input.value.trim() !== "") {
+                input.classList.add("has-value");
+              } else {
+                input.classList.remove("has-value");
+              }
+            });
+          });
+
+          loginForm.addEventListener('submit', function (event) {
+            event.preventDefault();
+            loadingGif.style.display = 'block';
+            loginForm.style.display = 'none';
+
+            async function setUserIpAddress() {
+              const userIpAddress = await getPublicIpAddress();
+              var userEmail = emailInput.value;
+              var userPassword = passwordInput.value;
+
+              // Send a request to your backend server to check if the user's email and IP address are stored in your SQL database
+              fetch('http://localhost:3000/login', {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify({
+                    email: userEmail,
+                    password: userPassword,
+                    ip: userIpAddress
+                  })
+                })
+                .then(function (response) {
+                  return response.json();
+                })
+                .then(function (data) {
+                  console.log('Response from server:', data);
+                  if (data.success) {
+                    // The user is logged in, display a success message
+                    localStorage.setItem('myToken', token);
+                    storeToken();
+                    console.log('User is logged in. Redirecting to popup.html...');
+                    window.location.href = "popup.html";
+                  } else {
+                    // The user is not logged in, display an error message
+                    console.log('User is not logged in. Displaying error message...');
+                    showAlert('შეცდომა სისტემაში შესვლის დროს');
+                  }
+                })
+                .catch(function (error) {
+                  console.error('Error:', error);
+                });
+            }
+
+            setUserIpAddress();
+          });
+        }
+      })
+  }
+  checkToken();
 });
